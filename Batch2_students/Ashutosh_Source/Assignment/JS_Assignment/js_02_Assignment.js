57b922e443104a4f478c5dfcee95e493b143fec4
Ashutosh more <ashutosh.more@expleogroup.com>
2024-09-13 16:44:48 +0530

    Second assignment of JS is completed

---
+++ Batch2_students/Ashutosh_Source/Assignment/JS_Assignment/js_02_Assignment.js
@@ -0,0 +1,48 @@
+// 1.Write  single line comment which says, comments can make code readable
+// this is code to print hello
+console.log("Hello");
+
+// 2.Write another single comment which says, Welcome to 30DaysOfJavaScript
+// Welcome to 30DaysOfJavaScript
+
+// 3.Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/*comments can make code readable, 
+easy to reuse and informative*/
+
+// 4.Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+let Astring = "Ashutosh More"
+let bboolean = true
+let cundefined = undefined
+let dnull = null
+
+// 5.Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+console.log(typeof(Astring));
+console.log(typeof(bboolean));
+console.log(typeof(cundefined));
+console.log(typeof(dnull));
+
+// 6.Declare four variables without assigning values
+let a 
+let b
+let c
+let d
+
+// 7.Declare four variables with assigned values
+let E = "Ashutosh"
+let f = 55
+let g = 66
+let h = true
+
+// 8.Declare variables to store your first name, last name, marital status, country and age in multiple lines
+firtname = "Ashutosh"
+lastname = "More"
+age = 26
+// 9.Declare variables to store your first name, last name, marital status, country and age in a single line
+firtname = "Ashutosh",lastname = "More",age = 26
+
+// 10.Declare two variables myAge and yourAge and assign them initial values and log to the console.?
+myage = 25
+yourage = 30
+console.log(myage);
+console.log(yourage);
+
