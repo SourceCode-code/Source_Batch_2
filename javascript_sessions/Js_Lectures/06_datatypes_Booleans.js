cf7f735a6daffd6073af84812a7520a7052071cd
KingThomas <akshay.kshirsagar2942@gmaiil.com>
2024-09-30 08:45:53 +0530

    Conversions & booleans

---
+++ javascript_sessions/Js_Lectures/06_datatypes_Booleans.js
@@ -0,0 +1,20 @@
+// datatypes ---
+
+// booelans -- booleans contains 2 values which are true or false 
+
+// use --> comparision 
+
+// True-->
+//1 all strings are true value expect (empty string )
+//2 all numbers are  true (postive & negative) expect (zero)
+
+
+//False-->
+//1 all  empty strings  value (empty string )
+//2 number (zero)
+//3 null
+//4 undefined 
+//5 NaN
+//6 On
+
+
