3e60328eb3cd0f0af99a9888048b42431163d455
Amol <jadhavamol.j2@gmail.com>
2024-10-01 13:44:24 -0400

    Please check Assignment no Js_06_07_amol.js

--- Batch2_students/Amol_Source/Assignment/JS_assignment/Js_06_07_amol.js
+++ Batch2_students/Amol_Source/Assignment/JS_assignment/Js_06_07_amol.js
@@ -4,33 +4,32 @@
 
 // 1. Check if the type of '10' is exactly equal to 10. If not, convert '10' to a number and check again.
 let strTen = '10';
-let numTen = 10;
+let numTen = 10 ;
 console.log( typeof(strTen));
 let x= (parseInt(strTen));
 console.log(typeof(x));
 
 
 // 2. Check if parseFloat('9.8') is equal to 10. If not, round it to make it equal to 10.
-console.log(parseFloat('9.8'))
-console.log(Math.round(9.8))
+console.log(parseFloat('9.8'));
+console.log(Math.round(9.8));
 
 
 // 3. Check if 'on' is found in both 'python' and 'jargon'.
 
 let word1 = 'python and jargon';
-console.log("Ans of Qes No. 3",word1.includes('on'))
-
+console.log("Ans of Qes No. 3",word1.includes('on'));
 
 
 // 4. Check if the word 'jargon' is found in the sentence: "I hope this course is not full of jargon."
 let sentence = "I hope this course is not full of jargon.";
-console.log("Ans of Qes No. 4",sentence.includes('jargon'))
+console.log("Ans of Qes No. 4",sentence.includes('jargon'));
 
 // 5. Use `substr` to slice out the phrase 'because because because' from the following sentence:
 // "You cannot end a sentence with because because because is a conjunction."
 let longSentence = 'You cannot end a sentence with because because because is a conjunction.';
-console.log("Ans of Qes No. 5",longSentence.substr(31,23));
-
+console.log(longSentence.substr(31,24))
+//console.log("Ans of Qes No. 5",longSentence.substr(indexof('becuase'),lastindexof('because')));
 
 // 6. Declare variables (firstName, lastName, country, city, age, isMarried, year) and use the `typeof` operator to check different data types.
 let firstName = "Amol"
@@ -55,52 +54,57 @@ console.log("Ans of Qes No.7",typeof(A));
 console.log("Ans of Qes No.7.1",typeof(B));
 
 // 8. Boolean value is either true or false. Write three JavaScript statements that provide truthy values.
-let fullName = 'Amol Kantilal jadhav';
-console.log("Ans of Qes No.8", A===A)
+console.log("Ans of Qes No.8", A==A);
 console.log("Ans of Qes No.8.1",(100>99));
 console.log("Ans of Qes No.8.2",(4>3)&&(3>0));
 
 // 9. Write three JavaScript statements that provide falsy values.
-console.log("Ans of Qes No.9",4>13 || 4<3 )
+console.log("Ans of Qes No.9",4>13 || 4<3 );
 console.log("Ans of Qes No.9.1",!(123<456));
-console.log("Ans of Qes No.9.2",3>0 && 4>7)
+console.log("Ans of Qes No.9.2",3>0 && 4>7);
 
 
 // 10. Figure out the result of the following comparison expressions without using `console.log()` first, then confirm the result using `console.log()`:
 //write the output in the comments 
-console.log("Ans of Qes No.10",4 > 3);       // true    . 4 is greater than 3
-console.log("Ans of Qes No.10.1",4 >= 3);    //true     . 4 is greater and equal to 3 
-console.log("Ans of Qes No.10.2",4 < 3);     //false    . 3 is not greater than 4 
-console.log("Ans of Qes No.10.3",4 <= 3);    //false    . 3 is not greater and equal to 4 
-console.log("Ans of Qes No.10.4",4 == 4);    //true     . here e check only value
-console.log("Ans of Qes No.10.5",4 === 4);   //true     . here the datatype and value also same
-console.log("Ans of Qes No.10.6",4 != 4);    //false    . its only provide opposite ans
-console.log("Ans of Qes No.10.7",4 !== 4);   //false    . here ans of == is true but ! make it false   
-console.log("Ans of Qes No.10.8",4 != '4');  //flase    . its only provide opposite ans
-console.log("Ans of Qes No.10.9",4 == '4');  // true    . here only check the value   
-console.log("Ans of Qes No.10.10",4 === '4');//flase    . here e check value with datatype 
+console.log("Ans of Qes No.10",4 > 3);        // true    . 4 is greater than 3
+console.log("Ans of Qes No.10.1",4 >= 3);     //true     . 4 is greater and equal to 3 
+console.log("Ans of Qes No.10.2",4 < 3);      //false    . 3 is not greater than 4 
+console.log("Ans of Qes No.10.3",4 <= 3);     //false    . 3 is not greater and equal to 4 
+console.log("Ans of Qes No.10.4",4 == 4);     //true     . here e check only value
+console.log("Ans of Qes No.10.5",4 === 4);    //true     . here the datatype and value also same
+console.log("Ans of Qes No.10.6",4 != 4);     //false    . its only provide opposite ans
+console.log("Ans of Qes No.10.7",4 !== 4);    //false    . here ans of == is true but ! make it false   
+console.log("Ans of Qes No.10.8",4 != '4');   //flase    . its only provide opposite ans
+console.log("Ans of Qes No.10.9",4 == '4');   // true    . here only check the value   
+console.log("Ans of Qes No.10.10",4 === '4'); //flase    . here e check value with datatype 
 
 // 11. Find the length of the words 'python' and 'jargon' and make a falsy comparison statement.
 let X ='python'
 let Y ='jargon'
 Z = X.length !== Y.length
-console.log("Ans of Qes No.11",Z)
+console.log("Ans of Qes No.11",Z);
 
 // 12. Evaluate the following expressions and confirm with `console.log()`:
 //write the output in the comments 
-console.log(4 > 3 && 10 < 12);   // true. here is both condition true
-console.log(4 > 3 && 10 > 12);   // flase. here the 10>12 condition as fail thats why flase in AND oprtaion
-console.log(4 > 3 || 10 < 12);   // true .  here is OR oprtaion thats why if one is true then its output is true
-console.log(4 > 3 || 10 > 12);   // true .  here is one condition is true thats why its output is true in OR oprators
-console.log(!(4 > 3));          //flase. becose ! showa always opposite output (Condition is true)but !
-console.log(!(4 < 3));           // True . becose ! show always opposite output (Condition is Flase)but !
-console.log(!(false));           //true .  ! always show opposite output
-console.log(!(4 > 3 && 10 < 12)); // flase . both condition is true but due to ! itd output is False
-console.log(!(4 > 3 && 10 > 12)); // True. in AND oprator if one condition is false then out put ill be flase . but ! oprator its true .
-console.log(!(4 === '4'));        // true . condition is fail but due to ! opratoe is shows true
+console.log("Ans of Qes No.12",4 > 3 && 10 < 12);   // true. here is both condition true
+console.log("Ans of Qes No.12.1",4 > 3 && 10 > 12);   // flase. here the 10>12 condition as fail thats why flase in AND oprtaion
+console.log("Ans of Qes No.12.2",4 > 3 || 10 < 12);   // true .  here is OR oprtaion thats why if one is true then its output is true
+console.log("Ans of Qes No.12.3",4 > 3 || 10 > 12);   // true .  here is one condition is true thats why its output is true in OR oprators
+console.log("Ans of Qes No.12.4",!(4 > 3));            //flase. bcoz ! showa always opposite output (Condition is true)but !
+console.log("Ans of Qes No.12.5",!(4 < 3));            //true . bcoz ! show always opposite output (Condition is Flase)but !
+console.log("Ans of Qes No.12.6",!(false));            //true .  ! always show opposite output
+console.log("Ans of Qes No.12.7",!(4 > 3 && 10 < 12)); // flase . both condition is true but due to ! itd output is False
+console.log("Ans of Qes No.12.8",!(4 > 3 && 10 > 12)); // True. in AND oprator if one condition is false then out put ill be flase . but ! oprator its true .
+console.log("Ans of Qes No.12.9",!(4 === '4'));        // true . condition is fail but due to ! opratoe is shows true
 
 // 13. Check if there is no 'on' in both 'dragon' and 'python'.
 let dragon = 'dragon';
+let python ='python'
+!dragon.includes('on') && python.includes('on')
+? console.log("Ans of Qes No.13",'on in both dragon and python')
+: console.log("Ans of Qes No.13",'no on in both dragon and python')
+
+ // ternary operator
 
 // 14. Write a script  the user to enter the base and height of a triangle, then calculate its area.
 let base = 20; 
@@ -109,36 +113,51 @@ let Area = .5 * 6* 8
 console.log("Ans of Qes No.14:Area",Area)
 
 // 15. Write a script that the user to enter sides a, b, and c of a triangle, then calculate its perimeter.
-let a = 3
-let b = 5
-let c = 7
+let a = 3;
+let b = 5;
+let c = 7;
 XYZ = a+b+c
-console.log("Ans of Qes No.15: perimeter",XYZ)
+console.log("Ans of Qes No.15: perimeter",XYZ);
 
 // 16. Calculate the area and perimeter of a rectangle, prompting for length and width.
-let length = 10
-let width = 20
+let length = 10;
+let width = 20;
 console.log("Ans of Qes No.15: Perimeter ",2*(length+width));
 console.log("Ans of Qes No.15: Area ",length*width);
 
 // 17. Get the radius   calculate the area and circumference of a circle.
 let cicleR = 9
-console.log (Math.round(Math.PI*cicleR**2))
+console.log ("Ans of Qes No.17", Math.round(Math.PI*cicleR**2));
 
 // 18. Calculate the slope, x-intercept, and y-intercept of y = 2x - 2.
+// Not able to solve
 
 // 19. Compare the slope between two points (2, 2) and (6, 10).
 let x1 = 2;
 let y1 = 2;
-let nx2 = 6;
-y2 = 10;
+let x2 = 6;
+let y2 = 10;
+let Slope = (6-2)/(10-2)
+console.log("Ans of Qes No.19",Slope);  // this formula i get it from chrome. bcoz im not able to remember it.
 
 // 20. Compare the slope of the two questions above.
 
 // 21. Calculate the value of y (y = x^2 + 6x + 9) for different x values and figure out when y equals 0.
 
 // 22. Compare the length of your first name and your family name.
+let firstname = "Amol";
+let lastname = "Jadhav";
+leng1 = (firstname.length) 
+leng2 = (lastname.length)
+console.log ("Ans of Qes No.22",leng2/leng1);
 
 // 23. Declare two variables `myAge` and `yourAge`, then calculate the age difference.
+let myAge = 24;
+let yourAge = 30;
+console.log("Ans of Qes No.23",yourAge-myAge);
 
 // 24. Using prompt, when birth year and calculate if they are old enough to drive.
+let Age = 17;
+Age > 18
+? console.log(`they are old enough to drive With age of ${Age}`)
+: console.log(`they are not old enough to drive With age of ${Age}`)
--- Batch2_students/Amol_Source/Assignment/JS_assignment/practice.js
+++ Batch2_students/Amol_Source/Assignment/JS_assignment/practice.js
@@ -1,52 +1,63 @@
-// // let Amol= 'Amol kantilal Jadhav'
-// // let sirName= 'Jadhav'
-// // let Job = 'Best Automation tester'
-// // console.log(Amol.substring(14,24))
-// // console.log(Amol.substr(14,10))
-// // console.log(Amol.replace('uyuyuyuyuy',' '));
-// // console.log(Amol.trim())
-// // console.log(Amol.length)
-// // let AmolJ=(Math.round((Math.random()*Amol.length)))
-// // console.log(Amol[AmolJ])
-// // console.log(Amol.repeat(2))
-// // console.log((Amol.repeat(2)).trim())
-// // console.log(Amol.concat(),sirName,Job)
-// // console.log( Amol +" "+ sirName +" "+ Job );
-// // let FullName=` ${Amol} ${sirName} ${Job}`
-// // console.log(FullName)
-// // console.log(Amol.includes("Amol"))
-// // console.log(Amol.startsWith('A'))
-// // console.log(Amol.endsWith('L'))
-// // console.log(Amol.substring(5,13))
-// // console.log(Amol.substr(5,9))
-// // let JAJA ='push push push push push "push\"'
-// // console.log(JAJA.split(" "))
+// // // let Amol= 'Amol kantilal Jadhav'
+// // // let sirName= 'Jadhav'
+// // // let Job = 'Best Automation tester'
+// // // console.log(Amol.substring(14,24))
+// // // console.log(Amol.substr(14,10))
+// // // console.log(Amol.replace('uyuyuyuyuy',' '));
+// // // console.log(Amol.trim())
+// // // console.log(Amol.length)
+// // // let AmolJ=(Math.round((Math.random()*Amol.length)))
+// // // console.log(Amol[AmolJ])
+// // // console.log(Amol.repeat(2))
+// // // console.log((Amol.repeat(2)).trim())
+// // // console.log(Amol.concat(),sirName,Job)
+// // // console.log( Amol +" "+ sirName +" "+ Job );
+// // // let FullName=` ${Amol} ${sirName} ${Job}`
+// // // console.log(FullName)
+// // // console.log(Amol.includes("Amol"))
+// // // console.log(Amol.startsWith('A'))
+// // // console.log(Amol.endsWith('L'))
+// // // console.log(Amol.substring(5,13))
+// // // console.log(Amol.substr(5,9))
+// // // let JAJA ='push push push push push "push\"'
+// // // console.log(JAJA.split(" "))
 
-// // Even = (Math.ceil(Math.random()*50))
-// // console.log(2*Even)
-// // Even1 = (Math.ceil(Math.random()*50))
-// // console.log((2*Even1)+1)
-// // let num1= 123456778
-// // console.log(typeof(toString(num1)))
+// // // Even = (Math.ceil(Math.random()*50))
+// // // console.log(2*Even)
+// // // Even1 = (Math.ceil(Math.random()*50))
+// // // console.log((2*Even1)+1)
+// // // let num1= 123456778
+// // // console.log(typeof(toString(num1)))
 
-// let Amol = "asks1234"
-// console.log(parseInt(Amol))
+// // let Amol = "asks1234"
+// // console.log(parseInt(Amol))
 
 
-// 22. Compare the length of your first name and your family name.
-let firstname = "Amol";
-let lastname = "Jadhav";
-leng1 = (firstname.length) 
-leng2 = (lastname.length)
-console.log (leng2/leng1);
+// // 22. Compare the length of your first name and your family name.
+// let firstname = "Amol";
+// let lastname = "Jadhav";
+// leng1 = (firstname.length) 
+// leng2 = (lastname.length)
+// console.log (leng2/leng1);
 
-// // 23. Declare two variables `myAge` and `yourAge`, then calculate the age difference.
-// let myAge = 24
-// let yourAge = 30
-// console.log(yourAge-myAge)
+// // // 23. Declare two variables `myAge` and `yourAge`, then calculate the age difference.
+// // let myAge = 24
+// // let yourAge = 30
+// // console.log(yourAge-myAge)
 
-// 24. Using prompt, when birth year and calculate if they are old enough to drive.
-let Age = 22;
-Age > 18
-? console.log(`they are old enough to drive With age of ${Age}`)
-:console.log(`they are not old enough to drive With age of ${Age}`)
+// // 24. Using prompt, when birth year and calculate if they are old enough to drive.
+// let Age = 22;
+// Age > 18
+// ? console.log(`they are old enough to drive With age of ${Age}`)
+// :console.log(`they are not old enough to drive With age of ${Age}`)
+// 13. Check if there is no 'on' in both 'dragon' and 'python'.
+// let dragon = 'dragon';
+// let python ='python'
+// !dragon.includes('on') || !python.includes('on')
+// ? console.log('on in both dragon and python')
+// : console.log('no on in both dragon and python')
+
+let longSentence = 'You cannot end a sentence with because because because is a conjunction.'
+
+console.log(longSentence.substr(31,24))
+//console.log("Ans of Qes No. 5",longSentence.substr(longSentence.indexOf('because'),longSentence.lastIndexOf('because')));
