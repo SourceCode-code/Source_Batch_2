0e9edd8a8761dd77370d4c8b81b98f4546fa98ae
KingThomas <akshay.kshirsagar2942@gmaiil.com>
2024-10-07 08:46:36 +0530

    loops

---
+++ javascript_sessions/Js_Lectures/10_loops.js
@@ -0,0 +1,172 @@
+// Loops --> loops are used to perform repitative actions 
+
+// for  -- used 99% of the time 
+// while -- used 1% of the time
+
+//1 for --> when the output is defeitive or fintie 
+
+/**
+ * syntax for loop (FOR)--->
+ * 
+ * for(intialzitse,condtion,increment/decrement)
+ * {
+ * your code 
+ * 
+ * }
+ * 
+ * 
+ */
+
+
+//example ---> 0-->10
+
+for(let i=0;i<=10;i++){
+console.log(i)
+}
+/**0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10*/
+
+// 10-->0
+
+for(let i=10;i>=0;i--){
+    console.log(i)
+}
+/**
+ * 10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+0
+ */
+
+//
+
+let str="siddhant"
+
+for(let i=0;i<=str.length;i++){
+    console.log(str[i])
+}
+/**
+ s
+i
+d
+d
+h
+a
+n
+t
+ */
+
+
+
+//----------------------------------------------------------------------------
+//2 x 1 = 2
+// 2 x 2 =4 
+
+
+// for(let i=2;i<=20;i++){
+
+
+// }
+
+//1 
+
+
+//10
+let num =2
+for(let i =1;i<=10;i++){
+console.log(`${num} x ${i}= ${num*i}`)
+}
+
+
+
+/**
+ *
+2 x 1= 2
+2 x 2= 4
+2 x 3= 6
+2 x 4= 8
+2 x 5= 10
+2 x 6= 12
+2 x 7= 14
+2 x 8= 16
+2 x 9= 18
+2 x 10= 20
+ */
+
+
+// print  the table of 5 
+let num1 = 5
+for(let a =1;a<=10;a++){
+    console.log(`${num1}x${a}=${num1*a}`)
+}
+
+/**
+ * 5x1=5
+5x2=10
+5x3=15
+5x4=20
+5x5=25
+5x6=30
+5x7=35
+5x8=40
+5x9=45
+5x10=50
+ */
+
+
+//while --->  used when the output is infitne ()
+
+/**
+ * 
+ * let i=0
+ * while(condition){
+ * code 
+ * increment/ decrement
+ * }
+ */
+
+
+// 0--5 
+
+let i=0
+while(i<=5){
+    console.log(i)
+    i++
+}
+/**
+ * 
+0
+1
+2
+3
+4
+5
+ */
+
+
+// 1-10 ( sqaure )
+//num x num 
+
+for(let i=1;i<=10;i++){
+    console.log(`${i*i}`)
+}
+
+
+// ------> 
+
