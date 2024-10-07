1c952df1bc9e409a00a3d3ee455e4aeb40bf541e
Atharva <panditatharva999@gmailgit config --global user.email you@example.comgit config --global user.email you@example.com.com>
2024-10-07 15:55:42 +0530

    changes done

---
+++ Batch2_students/Atharva_source/queries/Mock_int_assignment.js
@@ -0,0 +1,152 @@
+/// Notes on Agile and Scrum Methodology \\\\
+
+// Definintion :  Agile methodology or Agile model is a combination of iterative and incremental process .
+              
+
+//Iterative : Breaking the product into small  part & this is provided into sprint 
+              //(SPRINT: Repeating same process again & again)
+// Incremental : New Feature is implemented at the beginning & then more new features are added to existing software.
+
+// METHODOLOGIES Of AGILE : 
+/*
+ 1: SCRUM
+ 2: FEATURE DRIVEN DEVELOPMENT
+ 3:CRYSTAL METHODOLOGY
+ 4: EXTREME PROGRAMMING
+ 5: DYNAMIC SOFTWARE DEVELOPMENT METHOD
+ 6: LEAN SOFTWARE DEVELOPMENT 
+
+ // OUT OF ALL These Methodologies , only SCRUM is used NOWADAYS
+
+ // SCRUM : 1 :IT IS A AGILE DEVELOPMENT METHOD WHICH CONCENTRATES ON HOW TO MANAGE TASKS WITH TEAM BASED DEVELOPMENT ENVIRONMENT 
+           2: SCRUM Itself is a FRAMEWORK for effective team collaboration in complex Project.
+
+           3: SCRUM Consists of Three Roles : 
+              A) Product Owner 
+              B) SCRUM Master 
+              C) SCRUM TEAM 
+            
+              A) Product Owner : a) The Process of Scrum starts with PRODUCT OWNER. 
+                                 b) Product owner gets the input from the End user /StakeHolder and introduces the Product Backlog 
+
+              B) Scrum Master : A)Same as Project manager of that project .
+                                B) Scrum Master is responsible for setting of the metting of SPRINT MEETING.
+                                c)Removes any obstacles in the project .
+                                D)He coaches the team & Motivate.
+                                E)He is responsible to resolve any implementation and distraction for the Developer and QA Team . 
+
+              C) SCRUM TEAM : A) It consists of QA + Dev Team.
+                              B)Scrum Team Manages its own work an organizes to complete the work in particular sprint. 
+                              C) Team May consist of DESIGNER , DEVELOPER and TESTER
+             
+              D) SPRINT :  A)Sprint is a short, time-bound period of time when a development team works to complete a set of tasks.
+                           B)Sprint is the time period of 2-3 weeks during which a PRODUCT OWNER , SCRUM MASTER , SCRUM TEAM to develop 
+                              a software product 
+                           C)Generally, it is for 2-4 weeks depending upon the complexity of the project.
+                           D)Change request is applicable in the PRODUCT BACKLOG but not in the SPRINT  
+
+
+
+         
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
