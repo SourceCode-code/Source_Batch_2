44862a8c9d53354001ee0cbb4c5f0dd391d8d33e
KingThomas <akshay.kshirsagar2942@gmaiil.com>
2024-10-15 10:21:31 +0530

    js_12_assignment.js

---
+++ javascript_sessions/Js_Assignments/js_12_assignment.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ * //1  get me the largest number and the smallest number form the array 
+ * array=[1,2,3,4,5,6,7,8,9,11,22,55,88,9,99,44,55,2587452,256852,0,12585]
+
+//2  convert this array into uppcase 
+array=
+[ 'vaibhav', 'sneha', 'siddhant', 'shrinivas', 'athrava', 'Amol' ]
+
+//3 add 2 in every element of this array 
+numaarry=[1,2,3,4,5,6,7,8,9]
+
+//4 get the me sum of this array 
+
+numaarry=[1,2,3,4,5,6,7,8,9]
+
+//5 get me all the value in this array above 5
+
+numaarry=[1,2,3,4,5,6,7,8,9]
+
+//6 --- give me 4 examples of every method taught of array .
+* 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
