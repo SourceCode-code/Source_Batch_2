f352cf499af6bfe4bfc4784c3db43bde63549e53
SourceCode-code <siddhantsourceandcode@gmail.com>
2024-10-15 10:42:30 +0530

    Update js_12_assignment.js

--- javascript_sessions/Js_Assignments/js_12_assignment.js
+++ javascript_sessions/Js_Assignments/js_12_assignment.js
@@ -1,29 +1,18 @@
 /**
- * 
- * //1  get me the largest number and the smallest number form the array 
- * array=[1,2,3,4,5,6,7,8,9,11,22,55,88,9,99,44,55,2587452,256852,0,12585]
-
-//2  convert this array into uppcase 
-array=
-[ 'vaibhav', 'sneha', 'siddhant', 'shrinivas', 'athrava', 'Amol' ]
-
-//3 add 2 in every element of this array 
-numaarry=[1,2,3,4,5,6,7,8,9]
-
-//4 get the me sum of this array 
-
-numaarry=[1,2,3,4,5,6,7,8,9]
-
-//5 get me all the value in this array above 5
-
-numaarry=[1,2,3,4,5,6,7,8,9]
-
-//6 --- give me 4 examples of every method taught of array .
-* 
- * 
- * 
- * 
- * 
- * 
+ * 1) Find the largest and the smallest number from the array.
+ *    array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 55, 88, 9, 99, 44, 55, 2587452, 256852, 0, 12585]
+ *
+ * 2) Convert all elements of the following array to uppercase.
+ *    array = ['vaibhav', 'sneha', 'siddhant', 'shrinivas', 'athrava', 'Amol']
+ *
+ * 3) Add 2 to each element of the array.
+ *    numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+ *
+ * 4) Find the sum of all elements in the array.
+ *    numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+ *
+ * 5) Get all values in the array that are greater than 5.
+ *    numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+ *
+ * 6) Provide 4 examples of array methods that were taught, along with their explanations.
  */
-
