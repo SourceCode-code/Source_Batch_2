4d91f085fb10943bb5788a29e1f558c18fc49da2
Atharva <panditatharva999@gmailgit config --global user.email you@example.comgit config --global user.email you@example.com.com>
2024-11-20 12:39:37 +0530

    assign17 completed

--- Batch2_students/Atharva_source/Assignment/JS/Js_17_atharva.js
+++ Batch2_students/Atharva_source/Assignment/JS/Js_17_atharva.js
@@ -43,28 +43,22 @@ console.log("-----ex.level.1-Ans.2----------")
 // is_valid_variable('1first_name') # False
 // is_valid_variable('firstname') # True
 
+function is_valid_variable(variable) {
+    let regex  = /[^a-zA-z_$/][/^a-zA-Z_$0-9]*$/
+    return regex.test(variable)
+}
+
+console.log(is_valid_variable('first_name'))
+console.log(is_valid_variable('first-name'))
+console.log(is_valid_variable('1first_name'))
+console.log(is_valid_variable('firstname'))
+console.log("------Ex.levl.2-Ans-----------")
+
+
+
+
 
 // ```javascript
-// function tenMostFrequentWords(paragraph, n = 10) {
-//     let words = paragraph.toLowerCase().split(/\s+/);
-//     let wordCount = {};
-//     words.forEach(word => {
-//         word = word.replace(/[^\w]/g, '');
-//         if (wordCount[word]) {
-//             wordCount[word]++;
-//         } else {
-//             wordCount[word] = 1;
-//         }
-//     })
-
-//     let sortedWords = Object.keys(wordCount).sort((a, b) => wordCount[b] - wordCount[a]);
-//     let result = [];
-//     for (let i = 0; i < n && i < sortedWords.length; i++) {
-//         result.push({ word: sortedWords[i], count: wordCount[sortedWords[i]] });
-//     }
-//     return result;
-// }
-
 // function cleanText(text) {
 //     return text.replace(/[^a-zA-Z0-9\s]/g, '').toLowerCase();
 // }
@@ -74,18 +68,13 @@ console.log("-----ex.level.1-Ans.2----------")
 //     return tenMostFrequentWords(cleanedText, n);
 // }
 
-// let paragraph = `I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.`;
-// console.log(tenMostFrequentWords(paragraph));
 
-// let sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`;
-// console.log(cleanText(sentence));
-// console.log(mostFrequentWords(sentence));
 // ```
 // Exercises: Level 2
 
 // 1 Write a function called tenMostFrequentWords which get the ten most frequent word from a string?
 
-//     paragraph = `I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.`
+  let  Paragraph = `I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.`
 //     console.log(tenMostFrequentWords(paragraph))
 //     [
 //     {word:'love', count:6},
@@ -124,25 +113,47 @@ console.log("-----ex.level.1-Ans.2----------")
 // ]
 
 
-// function tenMostFrequentWords(paragraph, words) {
-//     let Paragraph = `I love teaching. If you do not love teaching what else can you love. 
-//                 I love Python if you do not love something which can give you 
-//                 all the capabilities to develop an application what else can you love.`
+// METHOD 1\\
 
-    
-//     let words = Paragraph.split(' ');
-//     let wordCount = {};
-//     words.forEach(word => {
-//         word = word.toLowerCase
-//         if (wordCount[word]) {
-//             wordCount[word]++;
-//         }
-//     })
+function tenMostFrequentWords(paragraph, n =10) {
+    let words = paragraph.toLowerCase().split(/\s+/);
+    let wordCount = {};
+    words.forEach(word => {
+        word = word.replace(/[^\w]/g, '');
+        if (wordCount[word]) {
+            wordCount[word]++;
+        } else {
+            wordCount[word] = 1;
+        }
+    })
 
+    let sortedWords = Object.keys(wordCount).sort((a, b) => wordCount[b] - wordCount[a]);
+    let result = [];
+    for (let i = 0; i < n && i < sortedWords.length; i++) {
+        result.push({ word: sortedWords[i], count: wordCount[sortedWords[i]] });
+    }
+    return result;
+}
+console.log(tenMostFrequentWords('I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love'))
+console.log("-----ex.lev.2.Method1------------")
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\\
 
-//------------Not sure about the logic about the above question ; NEED HELP for this !!-------------------- \\
+// METHOD 2\\
 
+function TenMostFrequentWords(Paragraph) {
+    const Words = Paragraph.toLowerCase().match(/\b\w+\b/g)
+    let WordsCount = {}
 
+    Words.forEach(Words => {
+        WordsCount[Words] = (WordsCount[Words] || 0) + 1
+    })
+
+    return Object.entries(WordsCount).map(([Words,count])=>({Words,count})).sort((a,b)=>b.count - a.count).slice(0,10)
+};
+console.log(TenMostFrequentWords('I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love'))
+console.log("-----ex.lev.2.Method-2----------")
+
+//--------------------------------------------------------------------------\\
 
 
 // Exercises: Level 3
@@ -152,20 +163,29 @@ console.log("-----ex.level.1-Ans.2----------")
 //   console.log(cleanText(sentence))
 //  I am a teacher and I love teaching There is nothing as more rewarding as educating and empowering people I found teaching more interesting than any other jobs Does this motivate you to be a teacher
 //  ```
-let sentence = entence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;
-                        I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`
-
-
-
-
-
-
-
-
-
+function clean(text){
+    //const sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`
+    return  text.replace(/[^\w\s]/g, "")
+    
+}
+let cleanedText = clean('%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`')
+    console.log(cleanedText)
+console.log("--------ex.lev.3.Ans-1---------------")
 
 // 2. Write a function which find the most frequent words. After cleaning, count three most frequent words in the string.
 
 // ```js
 //  console.log(mostFrequentWords(cleanedText))
 //  [{word:'I', count:3}, {word:'teaching', count:2}, {word:'teacher', count:2}]
+
+function mostFrequentWords(paragraph,limit){
+    const words = paragraph.toLowerCase().match(/\b\w+\b/g)
+    let wordsCount = {}
+
+    words.forEach(words =>{
+        wordsCount[words] = (wordsCount[words] || 0) + 1
+    })
+    return Object.entries(wordsCount).map(([words,count])=>({words,count})).sort((a ,b)=> b.count - a.count).slice(0,limit)
+}
+console.log(mostFrequentWords(cleanedText,10))
+console.log("-------ex.levl.3.Ans-2-----------")
