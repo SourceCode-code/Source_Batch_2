8790cb74e16be4877c919a6206ebfabf9b5828c9
KingThomas <akshay.kshirsagar2942@gmaiil.com>
2025-01-07 09:29:28 +0530

    playwright basics

---
+++ playwright/tests/01_basic.spec.js
@@ -0,0 +1,20 @@
+const {test,expect} = require("@playwright/test") // this line is used to import your commands and asserations 
+
+// testcases  --> single file to check feature 
+// test suite --> collection of silimar testcases used to check a feature 
+
+
+test("verify the basic testcases  structure of playwright",async({browser})=>{
+const Context = await browser.newContext() // this opens a new context in a browser
+const page = await browser.newPage() // this opens a new context in a browser
+
+// // visit a website 
+
+// await page.goto("https://webdriveruniversity.com/Contact-Us/contactus.html")
+
+
+
+ 
+}) 
+// actual way 
+// lean --> 
