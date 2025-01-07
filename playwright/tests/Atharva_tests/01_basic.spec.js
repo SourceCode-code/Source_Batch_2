50e38c96173c595b3aa26c2023b453ac04929fd1
Atharva <panditatharva999@gmailgit config --global user.email you@example.comgit config --global user.email you@example.com.com>
2025-01-07 10:33:09 +0530

    01_basic.spec.js updated

--- playwright/tests/Atharva_tests/01_basic.spec.js
+++ playwright/tests/Atharva_tests/01_basic.spec.js
@@ -1,9 +1,11 @@
 // const {test,expect} =require("@plawright/test");
-const {test,expect} = require("@playwright/test")
+const {test,expect} = require("@playwright/test") // line used to import assertions and commands
 
 test("Verify the basic testcase structure of Playwrigt ",async({browser})=>{
-    const Context  = await browser.newContext();
-    const page = await browser.newPage();
+    const Context  = await browser.newContext(); //opens a new context in the browser
+    const page = await browser.newPage(); // opens a new page in the browser
 
+
+    //visiting a page :-//
     await page.goto("https://aws.amazon.com/what-is/restful-api/")
 })
