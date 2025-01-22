d8df60183ccfc1adbb40d5fd4ddee42189e7482a
KingThomas <akshay.kshirsagar2942@gmaiil.com>
2025-01-22 09:35:29 +0530

    actions

---
+++ playwright/tests/07_Batch2_Alertsandpopups.spec.js
@@ -0,0 +1,30 @@
+const {test,expect}= require("@playwright/test")
+
+test("verify  js alerts",async({page})=>{
+
+//visit
+await page.goto("https://webdriveruniversity.com/Popup-Alerts/index.html")
+
+//click on btn
+await page.locator('[id="button4"]').click()
+// verify alert
+page.on("dialog",async (simplealert)=>{
+    await simplealert.message()
+    await expect (simplealert.message()).toContain("Press a button!")
+    await simplealert.accept()
+    console.log(simplealert.message())
+})
+
+await page.waitForTimeout(5000)
+let text=await page.locator('[id="confirm-alert-text"]').textContent()
+await expect(text).toContain("You pressed OK!")
+
+
+})
+
+
+
+
+
+
+
