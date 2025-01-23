d483d3934864292b06f2379f9f9d7383e0b44b42
KingThomas <akshay.kshirsagar2942@gmaiil.com>
2025-01-23 09:39:08 +0530

    file upload and download

---
+++ playwright/tests/08_Batch2_fileupload.spec.js
@@ -0,0 +1,25 @@
+const { test, expect } = require("@playwright/test")
+const exp = require("constants")
+
+test("Verify the upload functionality in playwright", async ({ page }) => {
+
+  
+    //visit
+    await page.goto("https://webdriveruniversity.com/File-Upload/index.html")
+    //upload the file
+    //playwright\Fixture\Fileuploaddata\SiddhantResume.pdf
+    await page.locator("#myFile").setInputFiles('../playwright/Fixture/Fileuploaddata/SiddhantResume.pdf')
+
+    page.on("dialog", async (alert) => {
+        await expect(alert.message()).toContain("Your file has now been uploaded!")
+        await alert.accept()
+    })
+
+    await page.locator('[id="submit-button"]').click()
+    await expect(page.url()).toContain("https://webdriveruniversity.com/File-Upload/index.html?filename=SiddhantResume.pdf")
+
+
+})
+
+
+
