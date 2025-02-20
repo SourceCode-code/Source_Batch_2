be7dd809e4655f9d707c9ee9bae717d41d2318a9
KingThomas <akshay.kshirsagar2942@gmaiil.com>
2025-02-20 10:02:17 +0530

    framework creations_3

--- Playwright_Framework_1/Support/PageObjects/ContactusPage.js
+++ Playwright_Framework_1/Support/PageObjects/ContactusPage.js
@@ -1,6 +1,9 @@
 const { test, expect } = require("@playwright/test")
 class ContactusPage {
     inputfeildslector = '[id="contact_form"] [class="feedback-input"]'
+    resetbtnselector = '[class="contact_button"][type="reset"]'
+    sumbitbtnselector = '[class="contact_button"][type="submit"]'
+    successmessage = '[id="contact_reply"] h1'
 
 
 
@@ -12,6 +15,23 @@ class ContactusPage {
         await page.locator(`[id="contact_form"] [class="feedback-input"][placeholder="${inputname}"]`).fill(text)
     }
 
+    //verify the input value in inputbox
+
+    async verifyInputvalueinTextbox(page, inputname, text) {
+        await expect(page.locator(`[id="contact_form"] [class="feedback-input"][placeholder="${inputname}"]`)).toHaveValue(text)
+    }
+
+
+    //verify sumbit btn 
+    async verifySumbitbtn(page) {
+        await page.locator(this.sumbitbtnselector).click()
+    }
+
+    //
+    async verifySuccessmessage(page, Message) {
+        let text = await page.locator(this.successmessage).textContent()
+        await expect(text).toContain(Message)
+    }
 }
 export const contactusPage = new ContactusPage()
 
