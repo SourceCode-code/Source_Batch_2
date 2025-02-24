f11b881b7ed170b4238e1668741709aefb7e20d7
KingThomas <akshay.kshirsagar2942@gmaiil.com>
2025-02-24 10:03:00 +0530

    excetion coomands

--- Playwright_Framework_1/playwright.config.js
+++ Playwright_Framework_1/playwright.config.js
@@ -6,6 +6,7 @@ module.exports = defineConfig({
   testDir: './tests',
   reporter: 'html', 
   timeout:30000, // 
+  fullyParallel: true,
   // report --> html , mochaawesome report , allure report
   use: {
     baseURL:"https://webdriveruniversity.com/",
@@ -13,6 +14,43 @@ module.exports = defineConfig({
     headless:false,
     viewport:null,
   },
+    // /* Configure projects for major browsers */
+    // projects: [
+    //   {
+    //     name: 'chromium',
+    //     use: { ...devices['Desktop Chrome'] },
+    //   },
+  
+    //   {
+    //     name: 'firefox',
+    //     use: { ...devices['Desktop Firefox'] },
+    //   },
+  
+    //   {
+    //     name: 'webkit',
+    //     use: { ...devices['Desktop Safari'] },
+    //   },
+  
+    //   /* Test against mobile viewports. */
+    //   // {
+    //   //   name: 'Mobile Chrome',
+    //   //   use: { ...devices['Pixel 5'] },
+    //   // },
+    //   // {
+    //   //   name: 'Mobile Safari',
+    //   //   use: { ...devices['iPhone 12'] },
+    //   // },
+  
+    //   /* Test against branded browsers. */
+    //   // {
+    //   //   name: 'Microsoft Edge',
+    //   //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    //   // },
+    //   // {
+    //   //   name: 'Google Chrome',
+    //   //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    //   // },
+    // ],
 
 });
 
